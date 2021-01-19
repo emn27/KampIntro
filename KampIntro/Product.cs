c8a6bd9b4bc7f66c5b6558137596e92a0787a391
eminebülbül <emn.bulbul@hotmail.com>
2021-01-19 23:55:20 +0300

    ilk kodum

---
+++ KampIntro/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KampIntro
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Type { get; set; }
+    }
+}
--- KampIntro/Program.cs
+++ KampIntro/Program.cs
@@ -6,7 +6,52 @@ namespace KampIntro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Product product1 = new Product();
+            product1.Name = "Elma";
+            product1.Type = "Amasya Elması";
+            product1.Price = 10;
+
+            Product product2 = new Product();
+            product2.Name = " ceviz";
+            product2.Price = 20;
+
+            Product[] products = new Product[] { product1, product2 };
+
+            
+            
+               
+              
+
+           
+
+
+          
+        
+          
+        
+       
+           
+       
+       
+
+       
+
+
+        
+        
+
+        
+
+
+            
+        
+
+        
+
+           
+             
+            
+            
         }
     }
 }
